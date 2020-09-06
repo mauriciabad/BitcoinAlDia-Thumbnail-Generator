5107343a5aa1defd2c4edfe556b57bccaabe01ae
mauriciabad <hello@mauriciabad.com>
2020-09-06 18:14:40 +0200

    wip

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,4 +1,26 @@
 /* eslint-disable no-console */
 import 'babel-polyfill';
+import html2canvas from 'html2canvas';
+// import templates from '../data/templates';
 
-import templates from '../data/templates';
+function download(filename, data) {
+  const element = document.createElement('a');
+
+  element.setAttribute('href', data);
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+html2canvas(document.querySelector('#thumbnail')).then((canvas) => {
+  const imageData = canvas.toDataURL('image/png');
+
+  const imgData = imageData.replace(/^data:image\/png/, 'data:application/octet-stream');
+
+  download(`filename-${1}.png`, imgData);
+});
